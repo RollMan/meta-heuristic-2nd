74cbbe839e725e60ad97c0147e6d98fcd67e86da
Yohei Shimmyo <qwpmb554@gmail.com>
2018-11-27 12:10:08 +0900

    remove sleep function

--- differentialevolution/f_1.m
+++ differentialevolution/f_1.m
@@ -4,4 +4,4 @@ x1 = params.parameter1(1);
 x2 = params.parameter2(1);
 x3 = params.parameter3(1);
 out = x1^2 + x2^2 + x3^2;
-pause(0.1);
+% pause(0.1);
--- differentialevolution/f_2.m
+++ differentialevolution/f_2.m
@@ -4,4 +4,4 @@ x1 = params.parameter1(1);
 x2 = params.parameter2(1);
 %val = x.^2 + y.^2 + z.^2;
 out = 100 * (x1^2 - x2)^2 + (1 - x1)^2;
-pause(0.1);
+% pause(0.1);
